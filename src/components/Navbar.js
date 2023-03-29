1f6ac34404d7dc5f18d706826a70fc19e52f920e
Mia Fallon <pop37corn@gmail.com>
2023-03-29 18:04:14 -0400

    Killed product page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,11 +62,11 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/">
+                Home
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
